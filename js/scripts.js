0b1b38611e3c47a58dc9a06c61772881a40bbf49
Crystal <crystalcontrerasgrossman@gmail.com>
2015-02-19 10:12:18 -0800

    add ability to input multiple addresses for a contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                '<div class="form-group">' +
+                                   '<label for="new-street1">Address Line 1</label>' +
+                                   '<input type="text" class="form-control new-street1" placeholder="Street Address, P.O. box, company name, c/o">' +
+
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                   '<label for"new-street2">Address Line 2</label>' +
+                                   '<input type="text" class="form-control new-street2" placeholder="Apartment, suite, unit, building, floor, etc.">' +
+
+                                   '</div>' +
+                                   '<div class="form-group">' +
+                                     '<label for="new-city">City</label>' +
+                                      '<input type="text" class="form-control new-city" placeholder="City">' +
+
+                                    '</div>' +
+                                    '<div class="form-group">' +
+                                      '<label for="new-state">State</label>' +
+                                      '<input type="text" class="form-control new-state" placeholder="State">' +
+
+                                      '</div>' +
+                                    '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
@@ -7,7 +31,7 @@ $(document).ready(function() {
 
     var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
 
-    $(".new-address").each(function() {
+    $(".new-address").each(function(){
       var inputtedStreet1 = $("input#new-street1").val();
       var inputtedStreet2 = $("input#new-street2").val();
       var inputtedCity = $("input#new-city").val();
@@ -25,12 +49,19 @@ $(document).ready(function() {
       };
       newContact.addresses.push(newAddress);
     });
-  };
 
     $("ul#contacts").append("<li><span class='contact'>" +
                            newContact.firstName + " " +
                            newContact.lastName +
                            "</span></li>");
+  });
+
+
+    $(".contact").last().click(function(){
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+      $(".address").text(newContact.address);
+    });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
@@ -40,10 +71,4 @@ $(document).ready(function() {
     $("input#new-state").val("");
     $("input#new-zip").val("");
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-      $(".address").text(newContact.address);
-    });
-  });
-);
+});
